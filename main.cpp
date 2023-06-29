ff2bbe2a08539931363135767b11bd48f07e2871
FlzGlz <51001772+richfls@users.noreply.github.com>
2023-06-29 09:49:16 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
+string name = "Ricardo";// Holds Ricardo
+string name;// Holds nothing but is a string
+
+float dec;
+float dec = .23;
+
+int number = 200;
+int number;
+
+bool on_off = true;
+bool on_off;
+
+cout << "hello what is your name" << endl;
+cin >> name;
 
 
-string name;
-
-cout<<"Give me a name"<<endl;
-cin>>name;
-
-Give me a name
-ricardo 
 
 
-ricardo
+
+
+
+
